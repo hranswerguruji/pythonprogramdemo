eadcdd4575d648f243c5be08065827b0cdfd447a
hranswerguruji <hranswerguruji@gmail.com>
2024-05-16 09:09:26 +0530

    Calling sub, mul and div functions in main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import CalculatorFunction
 
 add = CalculatorFunction.add(10, 5)
 print(f"The adding of 2 number is {add}")
+sub = CalculatorFunction.add(10, 5)
+print(f"The subtraction of 2 number is {sub}")
+mul = CalculatorFunction.add(10, 5)
+print(f"The multiplication of 2 number is {mul}")
+div = CalculatorFunction.add(10, 5)
+print(f"The division of 2 number is {div}")
