bbf55f2fad289dc459c6c494e4f2c19dcb3493fd
hranswerguruji <hranswerguruji@gmail.com>
2024-05-16 09:04:29 +0530

    Calling the add method in main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import FunctionDemo
+import CalculatorFunction
 
-FunctionDemo.first_demo()
+# FunctionDemo.first_demo()
+
+add = CalculatorFunction.add(10, 5)
+print(f"The adding of 2 number is {add}")
